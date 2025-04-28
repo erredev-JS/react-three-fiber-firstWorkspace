2efa85e89dc3590b27983e34876ae6be4939a809
erredev-JS <roddev100@gmail.com>
2025-04-27 21:24:23 -0300

    ImgSection implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Header } from './components/Header/Header'
 
 import { HeroTesla } from './components/HeroTesla/HeroTesla'
+import { PortraitImgSection } from './components/PortraitImgSection/PortraitImgSection'
 import TextSection from './components/TextSection/TextSection'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Header/>
     <HeroTesla/>
     <TextSection/>
+    <PortraitImgSection/>
     </>
   )
 }
---
+++ src/components/PortraitImgSection/PortraitImgSection.tsx
@@ -0,0 +1,15 @@
+import style from './PortraitImgSection.module.css'
+export const PortraitImgSection = () => {
+  return (
+    <div className={style.imgSection}>
+
+
+    <div className={style.scrollCard}>
+   <p className={style.cardTitle}> Ready for Adventure</p>
+   <p className={style.cardSubtitle}>Travel up to 563 km on a single charge — enough to take you into the wild and beyond. Regain up to 237 km of range with just 15 minutes of fast charging.</p>
+    </div>
+
+
+    </div>
+  );
+};
